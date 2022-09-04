2dd0f22f159bb6c37fdea19df5c6f7fa4988c9c1
wangjun <444055828@qq.com>
2022-09-04 23:48:19 +0800

    fix bugs

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,35 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_attribute]
 pub fn actix_header(attr: TokenStream, token: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(token);
     let ident = input.ident.clone();
-    let name = attr.to_string();
+    let name = attr.to_string().replace("\"", "");
     let output = quote!(
         #input
 
         use std::str::FromStr;
-        use actix_web::error::ParseError;
 
-        impl TryIntoHeaderValue for #ident {
-            type Error = InvalidHeaderValue;
-            fn try_into_value(self) -> Result<HeaderValue, Self::Error> {
+        impl actix_web::http::header::TryIntoHeaderValue for #ident {
+            type Error = actix_web::http::header::InvalidHeaderValue;
+
+            fn try_into_value(self) -> Result<actix_web::http::header::HeaderValue, Self::Error> {
+
                 let s: String = self.into();
-                HeaderValue::from_str(&s)
+                actix_web::http::header::HeaderValue::from_str(&s)
             }
 
         }
-        impl ParseHeader for #ident {
-            fn name() -> HeaderName {
-                HeaderName::from_str(#name).unwrap()
+        impl actix_web::http::header::Header for #ident {
+            fn name() -> actix_web::http::header::HeaderName {
+                actix_web::http::header::HeaderName::from_str(#name).unwrap()
             }
-            fn parse<M: HttpMessage>(msg: &M) -> Result<Self, ParseError> {
-                let s = msg.headers().get(Self::name()).ok_or(ParseError::Header)?.to_str().map_err(|e| {
-                    ParseError::Header
+
+            fn parse<M: actix_web::HttpMessage>(msg: &M) -> Result<Self, actix_web::error::ParseError> {
+                let s = msg.headers().get(#name).ok_or(actix_web::error::ParseError::Header)?.to_str().map_err(|e| {
+                    actix_web::error::ParseError::Header
                 })?;
                 Ok(Self::from(s.to_owned()))
             }
